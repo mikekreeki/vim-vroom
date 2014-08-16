37f1aa14fcf3e2cad55f9000e1a193414e7c7a18
Michal Krejčí <mikekreeki@gmail.com>
2014-08-16 20:19:55 +0200

    Give output a little more space

--- ruby/vim_formatter.rb
+++ ruby/vim_formatter.rb
@@ -2,8 +2,12 @@ require 'rspec/core/formatters/base_formatter'
 
 class VimFormatter < RSpec::Core::Formatters::BaseFormatter
   def dump_failures
+    output.puts "\n"
+
     failed_examples.each do |example|
       output.puts(RSpec::Core::Metadata::relative_path(example.location) + " #" + example.full_description.strip)
     end
+
+    output.puts "\n"
   end
 end
